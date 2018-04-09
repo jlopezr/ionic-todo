46f1cc0d7f3f63f5490c0d0aed2142d758f7c83b
Juan López Rubio <j.lopez.r@gmail.com>
2018-04-09 16:07:30 +0200

    Storage working

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {ModalController, NavController} from 'ionic-angular';
+import {AddItemPage} from "../add-item/add-item";
+import {ItemDetailPage} from "../item-detail/item-detail";
+import {Data} from "../../providers/data/data";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,50 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public items = [];
 
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController, public dataService: Data) {
+    this.dataService.getData().then((todos)=> {
+      if(todos) {
+        this.items = todos;
+      }
+    });
+  }
+
+  ionViewDidLoad() {
+    /*this.items = [
+      {title:'hi1', description:'test 1'},
+      {title:'hi2', description:'test 2'},
+      {title:'hi3', description:'test 3'}
+    ];
+    */
+  }
+
+  addItem() {
+
+    console.log("addItem()");
+
+    let addModal = this.modalCtrl.create(AddItemPage);
+
+    addModal.onDidDismiss((item) => {
+      console.log("Saving "+item);
+      if (item) {
+        this.saveItem(item);
+      }
+    });
+
+    addModal.present();
+  }
+
+  saveItem(item) {
+    this.items.push(item);
+    this.dataService.save(this.items);
+  }
+
+  viewItem(item) {
+    this.navCtrl.push(ItemDetailPage, {
+      item: item
+    });
   }
 
 }
